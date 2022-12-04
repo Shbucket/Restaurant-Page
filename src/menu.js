0c3b538dcda5bc510d8cf4f48fb754eafb3996bd
shoamri joseph <shomarijoseph@gmail.com>
2022-12-03 21:06:31 -0500

    created load menu function

---
+++ src/menu.js
@@ -0,0 +1,7 @@
+export function loadMenu(){
+    const content = document.getElementById('content')
+    const info = document.createElement('div')
+    const para = document.createElement('p')
+    const sponge = new Image()
+    sponge.src 
+}
