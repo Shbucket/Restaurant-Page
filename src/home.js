4907822f1a16c0a1b0bb42801809db5d14de8b45
shoamri joseph <shomarijoseph@gmail.com>
2022-12-03 19:53:49 -0500

    added krabby patty image

--- src/home.js
+++ src/home.js
@@ -1,15 +1,29 @@
-import bottom from './'
+import krab from "./Mr.Krabs.png";
+import patty from './krabby-patty.png'
 
-export  function loadHome() {
+export function loadHome() {
   const content = document.getElementById("content");
   const header = document.createElement("h1");
+  const info = document.createElement('div')
+
+  const krabs = new Image();
+  krabs.src = krab;
+  krabs.classList.add("Mr_Krabs");
+  content.appendChild(krabs);
+
+  content.appendChild(info)
   header.textContent = "Welcome to the Krusty Krab";
-  content.appendChild(header);
+  info.appendChild(header);
 
-  const para = document.createElement('p')
-  para.textContent = 'The krusty krab is the only place in Bikini Bottom where you can eat the world famous, krabby patty!';
-  content.appendChild(para)
- const Bikini = new Image()
- Bikini.src 
+  const para = document.createElement("p");
+  para.textContent =
+    "The krusty krab is the only place in Bikini Bottom where you can eat the world famous, krabby patty!";
+ info.appendChild(para);
 
+ const krabby = new Image();
+ krabby.src = patty
+ krabby.classList.add('patty')
+ info.appendChild(krabby)
+
+ 
 }
--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 import * as test from './home.js'
 import "./style.css";
-
 console.log("Hello This is coming from index.js");
 test.loadHome()
