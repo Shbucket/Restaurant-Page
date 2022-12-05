e2177f21e0dbd8371882b0e5155f486629e80958
shoamri joseph <shomarijoseph@gmail.com>
2022-12-05 15:56:06 -0500

    added font family

--- src/contact.js
+++ src/contact.js
@@ -4,14 +4,20 @@ export function loadContact() {
   const content = document.getElementById("content");
   content.replaceChildren();
 
+   const squid = new Image();
+   squid.src = squi;
+   squid.classList.add("mascot");
+   content.appendChild(squid);
+
  const info = document.createElement("div");
  info.classList.add("info");
  content.appendChild(info);
 
- const squid = new Image();
- squid.src = squi ;
- squid.classList.add("mascot");
- content.appendChild(squid);
+
  
+ const para = document.createElement('p')
+ para.textContent = 'You can find us downtown in Bikini Bottom at 831 Bottom Feeder Lane. Right across the street from the Chum bucket'
+ info.appendChild(para)
+
 
 }
